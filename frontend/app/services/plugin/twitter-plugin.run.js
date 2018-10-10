d55ce79c759a975debecf44916e3fadb260169fc
Christophe Hamerling <chamerling@linagora.com>
2018-10-10 11:19:35 +0200

    linagora/lgs/openpaas/linagora.esn.unifiedinbox#947 Fix the module name

--- frontend/app/services/plugin/twitter-plugin.run.js
+++ frontend/app/services/plugin/twitter-plugin.run.js
@@ -1,30 +1,30 @@
-(function() {
+(function(angular) {
   'use strict';
 
-  angular.module('linagora.esn.unifiedinbox')
+  angular.module('linagora.esn.unifiedinbox.twitter').run(inboxTwitterPlugin);
 
-    .run(function($q, inboxFilters, inboxPlugins, _, INBOX_TWITTER_TYPE, session) {
-      inboxPlugins.add({
-        type: INBOX_TWITTER_TYPE,
-        contextSupportsAttachments: _.constant($q.when(false)),
-        resolveContextName: function(account) {
-          return $q.when('@' + _.find(session.getProviderAccounts(INBOX_TWITTER_TYPE), { id: account }).username);
-        },
-        getEmptyContextTemplateUrl: _.constant($q.when('/unifiedinbox.twitter/app/services/plugin/twitter-plugin-empty-message.html'))
-      });
-         inboxFilters.add([{
-          id: 'inboxTwitterMentions',
-          displayName: 'Mentions',
-          type: INBOX_TWITTER_TYPE,
-          selectionById: true
-        },
-        {
-          id: 'inboxTwitterDirectMessages',
-          displayName: 'Direct Messages',
-          type: INBOX_TWITTER_TYPE,
-          selectionById: true
-        }
-      ]);
+  function inboxTwitterPlugin($q, inboxFilters, inboxPlugins, _, INBOX_TWITTER_TYPE, session) {
+    inboxPlugins.add({
+      type: INBOX_TWITTER_TYPE,
+      contextSupportsAttachments: _.constant($q.when(false)),
+      resolveContextName: function(account) {
+        return $q.when('@' + _.find(session.getProviderAccounts(INBOX_TWITTER_TYPE), { id: account }).username);
+      },
+      getEmptyContextTemplateUrl: _.constant($q.when('/unifiedinbox.twitter/app/services/plugin/twitter-plugin-empty-message.html'))
     });
+        inboxFilters.add([{
+        id: 'inboxTwitterMentions',
+        displayName: 'Mentions',
+        type: INBOX_TWITTER_TYPE,
+        selectionById: true
+      },
+      {
+        id: 'inboxTwitterDirectMessages',
+        displayName: 'Direct Messages',
+        type: INBOX_TWITTER_TYPE,
+        selectionById: true
+      }
+    ]);
+  }
 
-})();
+})(angular);

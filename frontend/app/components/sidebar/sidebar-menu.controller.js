3d2a0da9c624a026b931dc0eacedd3c6c1c4eb6c
Fabien MOYON <fmoyon@linagora.com>
2019-03-06 16:38:21 +0100

    linagora.esn.unifiedinbox.social#1 Update twitter sidebar menu

--- frontend/app/components/sidebar/sidebar-menu.controller.js
+++ frontend/app/components/sidebar/sidebar-menu.controller.js
@@ -2,15 +2,27 @@
   'use strict';
 
   angular.module('linagora.esn.unifiedinbox.twitter')
-    .controller('inboxTwitterSidebarMenuController', function(session, inboxConfig, INBOX_TWITTER_CONFIG_TWEETS, INBOX_TWITTER_TYPE) {
+    .controller('inboxTwitterSidebarMenuController', function($rootScope, session, inboxConfig, INBOX_TWITTER_CONFIG_TWEETS, INBOX_TWITTER_TYPE, INBOX_EVENTS) {
       var self = this;
 
       self.$onInit = $onInit;
+      self.$onDestroy = $onDestroy;
 
       function $onInit() {
         inboxConfig(INBOX_TWITTER_CONFIG_TWEETS).then(function(enabled) {
-          self.accounts = enabled ? session.getProviderAccounts(INBOX_TWITTER_TYPE) : [];
+          self.accounts = session.getProviderAccounts(INBOX_TWITTER_TYPE);
+          self.isEnabled = enabled;
         });
+      }
+
+      var removeStateListener = $rootScope.$on(INBOX_EVENTS.FILTER_SOCIAL_CHANGED, function(evt, data) {
+        if (data.type === INBOX_TWITTER_TYPE) {
+          self.isEnabled = data.enable;
+        }
+      });
+
+      function $onDestroy() {
+        removeStateListener();
       }
     });
 

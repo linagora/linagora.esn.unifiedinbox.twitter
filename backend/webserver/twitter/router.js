492b29b31c5101650d5614c0efff3b369b38fb79
Christophe HAMERLING <chamerling@linagora.com>
2019-06-17 09:33:55 +0200

    #14 Support CORS

--- backend/webserver/twitter/router.js
+++ backend/webserver/twitter/router.js
@@ -1,12 +1,13 @@
-'use strict';
-
-var express = require('express');
+const express = require('express');
+const cors = require('cors');
 
 module.exports = dependencies => {
   const router = express.Router(),
         auth = dependencies('authorizationMW'),
         controller = require('./controller')(dependencies),
         middleware = require('./middleware')(dependencies);
+
+  router.all('/twitter/*', cors({ origin: true, credentials: true }));
 
   router.get('/twitter/directmessages', auth.requiresAPILogin, middleware.getAccount, controller.getDirectMessages);
   router.get('/twitter/mentions', auth.requiresAPILogin, middleware.getAccount, controller.getMentions);
